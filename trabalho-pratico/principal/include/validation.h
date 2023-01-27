e10715cb18087bf6413a66890fefa3e835d51c6b
Diogo Matos <diogomatosemail@gmail.com>
2023-01-27 23:09:21 +0000

    Bug fixes (#28)

--- trabalho-pratico/principal/include/validation.h
+++ trabalho-pratico/principal/include/validation.h
@@ -7,7 +7,8 @@ int validate_date(char *date);
 void to_lower_case(char *s);
 int validate_car_class(char *car_class);
 int validate_account_status(char *account_status);
-int validate_distance(int distance);
+int validate_distance(char *distance);
+int validate_score(char *score);
 int validate_number(char *tip);
 
 #endif
