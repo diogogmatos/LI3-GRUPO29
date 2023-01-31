afa269bf7d4535db6f0dd9ec3eafe8fedda83633
jose-freitas <a96140@alunos.uminho.pt>
2023-01-31 13:57:10 +0000

    query_info finished

--- trabalho-pratico/principal/src/interactive.c
+++ trabalho-pratico/principal/src/interactive.c
@@ -22,12 +22,45 @@ QUERY *query_info()
     queries[0].output = "nome;genero;idade;avaliacao_media;numero_viagens;total_gasto";
     queries[0].example = "João;M;23;4.5;10;100";
 
-    queries[1].description = "";
-    queries[1].arguments = "";
-    queries[1].output = "";
-    queries[1].example = "";
+    queries[1].description = "Lista os N condutores com maior avaliação média.";
+    queries[1].arguments = "<N> - Número de condutores a ser listados.";
+    queries[1].output = "nome;genero;idade;avaliacao_media;numero_viagens;total_gasto";
+    queries[1].example = "000000008899;Rafaela de Barros;3.564";
 
-    // queries[2] ...
+    queries[2].description = "Lista os N utilizadores com maior distância viajada.";
+    queries[2].arguments = "<N> - Número de utilizadores a ser listados.";
+    queries[2].output = "username;nome;distancia_total";
+    queries[2].example = "Anita-PetPinto38;Anita-Petra Pinto;240";
+    
+    queries[3].description = "Preço médio das viagens (sem considerar gorjetaas) numa determinada cidade.";
+    queries[3].arguments = "<city> - Nome da cidade.";
+    queries[3].output = "preco_medio";
+    queries[3].example = "10.057";
+    
+    queries[4].description = "Preço médio das viagens (sem considerar gorjetas) num dado intervalo de tempo.";
+    queries[4].arguments = "<data A> <data B> - Datas entre as quais a query atua.";
+    queries[4].output = "preco_medio";
+    queries[4].example = "10.107";
+  
+    queries[5].description = "Distância média percorrida, numa determinada cidade.";
+    queries[5].arguments = "<city> <data A> <data B> - Nome da cidade e datas entre as quais a query atua.";
+    queries[5].output = "distancia_media";
+    queries[5].example = "9.667";
+    
+    queries[6].description = "Top N condutores numa determinada cidade.";
+    queries[6].arguments = "<N> <city> - N condutores a ser listados e nome da cidade.";
+    queries[6].output = "id;nome;avaliacao_media";
+    queries[6].example = "000000001488;Rita Correia;4.250";
+
+    queries[7].description = "Lista todas as viagens nas quais os utilizadores e o condutor são do género passado como parâmetro e têm perfis com X ou mais anos.";
+    queries[7].arguments = "<gender> <X> - Gênero dos utilizadores e condutor e anos mínimos dos seus perfis.";
+    queries[7].output = "id_condutor;nome_condutor;username_utilizador;nome_utilizador";
+    queries[7].example = "000000009452;Ricardo Figueiredo;WiPinheiro;Wilson de Pinheiro";
+    
+    queries[8].description = "Lista as viagens nas quais o passageiro deu gorjeta, no intervalo de tempo (data A, data B).";
+    queries[8].arguments = "<data A> <data B> - Datas entre as quais a query atua.";
+    queries[8].output = "id_viagem;data_viagem;distancia;cidade;valor_gorjeta";
+    queries[8].example = "000000295454;01/01/2021;13;Faro;3.000";
 
     return queries;
 }
